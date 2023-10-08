b90026e58df401bed44c57dce30da805bdce77ec
Murray Gibbon <u20440716@tuks.co.za>
2023-10-08 22:12:20 +0200

    Updates

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+isBit: boolean =false;
+isCompSci: boolean = false;
+isBscIt: boolean = false;
+isCompEng: boolean = false;
+isInformatics: boolean=false;
+isMultimedia: boolean=false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isBit = this.router.url === '/BIT';
+        this.isCompSci = this.router.url === '/CompSci';
+        this.isBscIt = this.router.url === '/BScIT';
+        this.isCompEng = this.router.url === '/CompEng';
+        this.isInformatics = this.router.url === '/Informatics';
+        this.isMultimedia = this.router.url === '/Multimedia';
+      }
+    });
+  }
 }
