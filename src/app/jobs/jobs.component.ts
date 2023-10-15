88f4352d38bde32ccecf2b82408d9cb947b58f4c
AmickeC <croucampa01@gmail.com>
2023-10-15 12:49:37 +0200

    css updated & links

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-jobs',
@@ -9,10 +11,16 @@ import { Location } from '@angular/common';
 export class JobsComponent {
 
   
-  constructor(private location: Location) {}
+  constructor(private location: Location, private router: Router) {}
 
  
     goBack(): void {
       this.location.back(); 
     }
+
+    navigateToExternalURL() {
+      const externalURL = 'https://www.datafin.com/job/junior-software-developer-c-net-mvc-ui-ux/?action=register&vacid=24295';
+      window.open(externalURL, '_blank'); // Open in a new tab or window
+    }
+    
 }
